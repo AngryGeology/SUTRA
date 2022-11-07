e4b3ec96fd7b2cc75190129899c9174665b00283
Jimmy Boyd <j.boyd1@lancaster.ac.uk>
2022-11-07 10:18:57 +0000

    attempting compile with fast math enabled

--- compile_windows.bat
+++ compile_windows.bat
@@ -4,7 +4,7 @@ gfortran --std legacy -c fmods_3_0.f && ^
 gfortran --std legacy *.o -c lsubs_3_0.f && ^
 gfortran --std legacy -c ssubs_3_0.f  && ^
 gfortran --std legacy -c usubs_3_0.f && ^
-gfortran -static --std legacy *.o sutra_3_0.f -o ../bin/sutra.exe && ^
+gfortran -static -ffast-math --std legacy *.o sutra_3_0.f -o ../bin/sutra.exe && ^
 del *.mod && ^
 del *.o && ^
 cd .. 
