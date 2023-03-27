aaf26bb2ac6e5e729641a5b71b9da93e7a51951b
Jimmy Boyd <j.boyd1@lancaster.ac.uk>
2023-03-27 21:01:57 +0100

    sutra exe now tuned for avx2-core processors

--- compile_ifx.bat
+++ compile_ifx.bat
@@ -1,10 +1,10 @@
 cd source && ^
-ifx lmods_3_0.f /c && ^
-ifx fmods_3_0.f /c && ^
-ifx *.obj lsubs_3_0.f /c && ^
-ifx ssubs_3_0.f /c && ^
-ifx usubs_3_0.f /c && ^
-ifx *.obj sutra_3_0.f /exe:../bin/sutra.exe && ^
+ifx /arch:CORE-AVX2 lmods_3_0.f /c && ^
+ifx /arch:CORE-AVX2 fmods_3_0.f /c && ^
+ifx /arch:CORE-AVX2 *.obj lsubs_3_0.f /c && ^
+ifx /arch:CORE-AVX2 ssubs_3_0.f /c && ^
+ifx /arch:CORE-AVX2 usubs_3_0.f /c && ^
+ifx *.obj sutra_3_0.f /arch:CORE-AVX2 /exe:../bin/sutra.exe && ^
 del *.mod && ^
 del *.obj && ^
 cd .. 
