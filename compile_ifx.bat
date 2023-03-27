107c81409db596883f2feafdeacb627960febe7b
Jimmy Boyd <j.boyd1@lancaster.ac.uk>
2023-03-27 21:13:17 +0100

    alright, fastest possible version of windows sutra has been compiled ...

--- compile_ifx.bat
+++ compile_ifx.bat
@@ -1,11 +1,10 @@
 cd source && ^
-ifx /arch:CORE-AVX2 lmods_3_0.f /c && ^
-ifx /arch:CORE-AVX2 fmods_3_0.f /c && ^
-ifx /arch:CORE-AVX2 *.obj lsubs_3_0.f /c && ^
-ifx /arch:CORE-AVX2 ssubs_3_0.f /c && ^
-ifx /arch:CORE-AVX2 usubs_3_0.f /c && ^
-ifx *.obj sutra_3_0.f /arch:CORE-AVX2 /exe:../bin/sutra.exe && ^
+ifx /Ot /Oa lmods_3_0.f /c && ^
+ifx /Ot /Oa fmods_3_0.f /c && ^
+ifx /Ot /Oa *.obj lsubs_3_0.f /c && ^
+ifx /Ot /Oa ssubs_3_0.f /c && ^
+ifx /Ot /Oa usubs_3_0.f /c && ^
+ifx *.obj sutra_3_0.f /Ot /Oa /exe:../bin/sutra.exe && ^
 del *.mod && ^
 del *.obj && ^
 cd .. 
-
