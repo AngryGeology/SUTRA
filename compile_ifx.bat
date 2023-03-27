b26ee349d5f22063dbaadaa5c12431298e08e1c9
Jimmy Boyd <j.boyd1@lancaster.ac.uk>
2023-03-27 18:40:39 +0100

    windows exe updates ...

--- compile_ifx.bat
+++ compile_ifx.bat
@@ -1,10 +1,10 @@
 cd source && ^
-ifx -c lmods_3_0.f && ^
-ifx -c fmods_3_0.f && ^
-ifx *.obj -c lsubs_3_0.f && ^
-ifx -c ssubs_3_0.f  && ^
-ifx -c usubs_3_0.f && ^
-ifx -static *.obj sutra_3_0.f -o ../bin/sutra.exe && ^
+ifx lmods_3_0.f /c && ^
+ifx fmods_3_0.f /c && ^
+ifx *.obj lsubs_3_0.f /c && ^
+ifx ssubs_3_0.f /c && ^
+ifx usubs_3_0.f /c && ^
+ifx *.obj sutra_3_0.f /exe:../bin/sutra.exe && ^
 del *.mod && ^
 del *.obj && ^
 cd .. 
